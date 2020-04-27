84c57a81aecf212ec42d9bd658a83ec1a749e6e1
saismaran <saismaranhotaeducation@gmail.com>
2020-04-27 22:30:48 +0530

    sai test commit

--- AutomationTravelAgency/src/main/java/com/spring/AutomationTravelApp.java
+++ AutomationTravelAgency/src/main/java/com/spring/AutomationTravelApp.java
@@ -13,5 +13,6 @@ public class AutomationTravelApp
         SpringApplication.run(AutomationTravelApp.class, args);
         System.out.print("Gourav Check");
         System.out.print("Ritika Checked");
+        System.out.println("sai checked");
     }
 }
