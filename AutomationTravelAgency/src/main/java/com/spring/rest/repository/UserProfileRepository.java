ad118ce91c8164d930eb194f41fe9813e6631fbe
saismaran <saismaranhotaeducation@gmail.com>
2020-04-29 22:34:45 +0530

    sqlFile edit

--- AutomationTravelAgency/src/main/java/com/spring/rest/repository/UserProfileRepository.java
+++ AutomationTravelAgency/src/main/java/com/spring/rest/repository/UserProfileRepository.java
@@ -1,10 +1,12 @@
 package com.spring.rest.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Component;
 
 import com.spring.entity.UserCredentialsEntity;
 import com.spring.entity.UserProfileEntity;
 
+@Component
 public interface UserProfileRepository extends JpaRepository<UserProfileEntity, Long>  {
 
 }
