b0cb25eabbc913a4155936c03eae8e167bc299ce
Anurag-hub <60914815+Anurag-hub@users.noreply.github.com>
2020-04-29 15:56:11 +0530

    admin controller ,admin service,admin service impl done

--- AutomationTravelAgency/src/main/java/com/spring/controller/AdminCredentialsController.java
+++ AutomationTravelAgency/src/main/java/com/spring/controller/AdminCredentialsController.java
@@ -1,5 +1,6 @@
 package com.spring.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,6 +14,7 @@ import com.spring.service.AdminCredentialsService;
 @RequestMapping("/app/admin/")
 public class AdminCredentialsController {
 	
+	@Autowired
 	private AdminCredentialsService admincredentialsservice;
 
 	@RequestMapping(value="/vehicle",method=RequestMethod.POST, produces=MediaType.APPLICATION_JSON_VALUE, consumes=MediaType.APPLICATION_JSON_VALUE)
