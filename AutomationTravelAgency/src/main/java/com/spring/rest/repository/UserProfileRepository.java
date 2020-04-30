f4e126336d19beabadddddc64b29de363c865afa
Gourav <agouravsonu768@gmail.com>
2020-04-30 05:57:16 +0530

    "Commited USer Repository"

--- AutomationTravelAgency/src/main/java/com/spring/rest/repository/UserProfileRepository.java
+++ AutomationTravelAgency/src/main/java/com/spring/rest/repository/UserProfileRepository.java
@@ -2,22 +2,10 @@
 package com.spring.rest.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.spring.entity.UserCredentialsEntity;
-import com.spring.entity.UserProfileEntity;
-
-public interface UserProfileRepository extends JpaRepository<UserProfileEntity, Long>  {
-
-}
-=======
-package com.spring.rest.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Component;
 
-import com.spring.entity.UserCredentialsEntity;
 import com.spring.entity.UserProfileEntity;
-
+ 
 @Component
 public interface UserProfileRepository extends JpaRepository<UserProfileEntity, Long>  {
 
