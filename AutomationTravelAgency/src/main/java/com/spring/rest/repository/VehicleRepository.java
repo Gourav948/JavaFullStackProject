a6541adfdd7acaa6d1f7dafa7115bb6de8a5a6b4
Gourav <agouravsonu768@gmail.com>
2020-04-30 07:16:07 +0530

    "My Recent Changes"

--- AutomationTravelAgency/src/main/java/com/spring/rest/repository/VehicleRepository.java
+++ AutomationTravelAgency/src/main/java/com/spring/rest/repository/VehicleRepository.java
@@ -17,6 +17,8 @@ public interface VehicleRepository extends JpaRepository<VehicleEntity, Long>
 {
 
 	void deleteById(Long vehicleid);
+
+	VehicleEntity getById(Long vehicleid);
  
 
 }
---
+++ AutomationTravelAgency/src/main/java/com/spring/service/UserServiceImpl.java
@@ -0,0 +1,62 @@
+package com.spring.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.spring.json.Route;
+import com.spring.json.UserCredentials;
+import com.spring.rest.repository.RouteRepository;
+import com.spring.rest.repository.UserCredentialsRepository;
+import com.spring.utils.RouteUtils;
+import com.spring.utils.UserCredentialsUtils;
+import com.spring.entity.RouteEntity;
+import com.spring.entity.UserProfileEntity;
+import com.spring.entity.VehicleEntity;
+import com.spring.json.UserProfile;
+import com.spring.json.Vehicle;
+import com.spring.rest.repository.UserProfileRepository;
+import com.spring.rest.repository.VehicleRepository;
+import com.spring.rest.repository.UserCredentialsRepository;
+import com.spring.utils.UserProfileUtils;
+import com.spring.utils.VehicleUtils;
+
+
+ 
+@Service
+public class UserServiceImpl implements UserService  
+{
+	@Autowired
+	VehicleRepository vehicleRepository;
+	
+	@Autowired
+	RouteRepository routeRepository;
+	
+	@Override
+	public Object getVehicleById(Long vehicleid) 
+	{
+		VehicleEntity vehicleEntity=vehicleRepository.getById(vehicleid);
+		if(vehicleEntity!=null)
+			return VehicleUtils.convertVehicleEntityToVehicle(vehicleEntity);
+		else
+			return "Invalid VehicleId";
+	}
+
+	@Override
+	public Object getRouteById(Long routeId)
+	{
+		RouteEntity routeEntity=routeRepository.getById(routeId);
+		if (routeEntity!=null)
+		
+		return RouteUtils.convertRouteEntityToRoute(routeEntity);
+		else
+			return "Invalid RouteId please Check ID";
+	}
+ 
+
+
+
+	
+}
+
+
+
