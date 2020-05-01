c22dad467cdef010b3216d21c10616542e5450fb
saismaran <saismaranhotaeducation@gmail.com>
2020-05-01 14:23:54 +0530

    Registered userRegister service on UserCredential controller"

--- AutomationTravelAgency/src/main/java/com/spring/controller/UserCredentialsController.java
+++ AutomationTravelAgency/src/main/java/com/spring/controller/UserCredentialsController.java
@@ -8,12 +8,20 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
+<<<<<<< HEAD
 import com.spring.json.UserProfile;
 import com.spring.service.UserCredentialsService;
 //import com.spring.entity.PasswordResetEntity;
  
+=======
+import com.spring.entity.UserCredentialsEntity;
+import com.spring.json.UserCredentials;
+import com.spring.json.UserProfile;
+import com.spring.service.UserCredentialsService;
+>>>>>>> Registered userRegister service on UserCredential controller"
 
 @RestController
 @RequestMapping("/app/customer/")
@@ -46,21 +54,29 @@ public class UserCredentialsController {
 			
 			return value;
 		}
+<<<<<<< HEAD
 		*/
+=======
 		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
+		private UserCredentialsService userCredentialsService;
+
+		@PostMapping(value="/app/customer", produces=MediaType.APPLICATION_JSON_VALUE, consumes=MediaType.APPLICATION_JSON_VALUE)
+		public @ResponseBody UserProfile registerUserProfile(@RequestBody UserProfile userProfile) {
+			return userCredentialsService.save(userProfile);
+		}
 }
+>>>>>>> Registered userRegister service on UserCredential controller"
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	
 
